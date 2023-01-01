f8bb36204ccc6bc41db65aaf26efa0037199cb48
ifenna-pascal <ifennamonanu70@gmail.com>
2023-01-01 18:09:48 +0100

    feat: added metamask connection checker

--- src/context/contract.context.tsx
+++ src/context/contract.context.tsx
@@ -1,6 +1,6 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import {  contractProps, Props } from "./interface";
-import { connectWallet } from "./methods";
+import { confirmWalletConnection, connectWallet } from "./methods";
 
 // export conntext
 export const ContractContext = createContext<contractProps | null>(null);
@@ -8,10 +8,14 @@ export const ContractContext = createContext<contractProps | null>(null);
 export const ContractProvider = ({ children }: Props) => {
   const [account, setAccount] = useState<string>('');
   
+  useEffect(() => {
+    confirmWalletConnection(setAccount);
+  }, [account])
+
   const walletConnect = () => {
     connectWallet(setAccount);
   }
-
+  
   return (
     <ContractContext.Provider value={{walletConnect, account}}>
       {children}
--- src/context/methods.ts
+++ src/context/methods.ts
@@ -16,7 +16,26 @@ export const connectWallet = async (
     setAccount(accounts[0]);
     toast.success("Account connected successfully");
   } catch (e: any) {
-    console.log(e);
+    toast.error(e.message);
+  }
+};
+
+export const confirmWalletConnection = async (
+  setAccount: React.Dispatch<React.SetStateAction<string>>
+): Promise<undefined> => {
+  try {
+    const { ethereum } = window;
+    if (!ethereum) {
+      toast.error("Pls Install Metamask");
+      return;
+    }
+    const accounts = (await ethereum.request({
+      method: "eth_requestAccounts",
+    })) as string[];
+    if (accounts.length > 0) {
+      setAccount(accounts[0]);
+    }
+  } catch (e: any) {
     toast.error(e.message);
   }
 };
